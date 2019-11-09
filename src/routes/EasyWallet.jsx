50e0c408bd19a3d17af25a5b92cea464653b455d
Idsarth <idsarthdev19@gmail.com>
2019-11-09 09:24:13 -0600

    todo listo para empezar a trabajar

--- src/routes/EasyWallet.jsx
+++ src/routes/EasyWallet.jsx
@@ -4,7 +4,6 @@ import { Router } from '@reach/router'
 // Import pages
 const Home = React.lazy(() => import('../pages/Home/Home'))
 
-
 const EasyWallet = () => {
   return (
     <Suspense fallback={<div>loading...</div>}>
