75b1a657e5e9e8dcc7b74b46d59dd2d774b1198d
Idsarth <idsarthdev19@gmail.com>
2019-11-09 23:19:35 -0600

    preparando la funcion para obtener toda la lista de productos

--- src/pages/Product/ProductPage.jsx
+++ src/pages/Product/ProductPage.jsx
@@ -1,16 +1,53 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Table } from 'antd'
 import { useSelector, useDispatch } from 'react-redux'
 
 // Import components
 import AddProdcut from '../../components/Product/AddProduct'
 
+// Import utils
+import { apiUrl } from '../../utils/api'
+
 // Import actions
 import { PRODUCTS } from '../../store/actionTypes'
 
+const columns = [
+  {
+    title: '',
+    dataIndex: 'name'
+  },
+  {
+    title: 'Age',
+    dataIndex: 'age'
+  },
+  {
+    title: 'Address',
+    dataIndex: 'address'
+  }
+]
+
 const ProductPage = props => {
   const dispatch = useDispatch()
+  const [isLoading, setIsLading] = useState(false)
+  const { userName, userToken } = useSelector(state => state.user)
   const { message } = useSelector(state => state.product)
-  console.log(message)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLading(true)
+    
+      const response = await fetch(`${apiUrl}/${userName}/products`, {
+        method: 'GET',
+        headers: {
+          'Authorization': `Bearer ${userToken}`,
+          'Content-type': 'application/json'
+        }
+      })
+      const result = await response.json()
+      console.log(result)
+    }
+    fetchData()
+  }, [])
 
   const handleOnClose = () => {
     dispatch({
@@ -31,6 +68,9 @@ const ProductPage = props => {
         </div>
       )}
       <AddProdcut />
+      <div className='product-table'>
+        
+      </div>
     </div>
   )
 }
