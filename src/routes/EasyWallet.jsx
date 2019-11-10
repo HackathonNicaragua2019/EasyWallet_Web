e32f9b88348a0b07adc30536ac9806b115bfc1af
Idsarth <idsarthdev19@gmail.com>
2019-11-09 18:24:21 -0600

    error corregido en las validaciones en sigin

--- src/routes/EasyWallet.jsx
+++ src/routes/EasyWallet.jsx
@@ -8,32 +8,28 @@ import Profile from '../pages/Profile/ProfilePage'
 import DashboardHome from '../components/Dashboard/DashboardHome'
 const Home = React.lazy(() => import('../pages/Home/HomePage'))
 const Dashboard = React.lazy(() => import('../pages/Dashboard/DashboardPage'))
-const Mision = React.lazy(()=>import('../pages/Home/Mision'))
-const Vision = React.lazy(()=>import('../pages/Home/Vision'))
-const Valores = React.lazy(()=>import('../pages/Home/Valores'))
-
+const Mision = React.lazy(() => import('../pages/Home/Mision'))
+const Vision = React.lazy(() => import('../pages/Home/Vision'))
+const Valores = React.lazy(() => import('../pages/Home/Valores'))
 
 const EasyWallet = () => {
   return (
     <Suspense fallback={<Loading />}>
       <Router>
-<<<<<<< HEAD
         {/* <Redirect from='/' to='signin' /> */}
         <Home path='/' />
         <SignIn path='signin' />
         <Dashboard path='/dashboard' />
-        <Mision path='/mision'/>
-        <Vision path='/vision'/>
-        <Valores path="/valores"/>
+        <Mision path='/mision' />
+        <Vision path='/vision' />
+        <Valores path='/valores' />
       
-=======
         <Home path='/' />
         <SignIn path='signin' />
         <Dashboard path='dashboard'>
           <DashboardHome path='/' />
           <Profile path='profile' />
         </Dashboard>
->>>>>>> 0204e55a34cd92022710d63cabf24c1cb9e73dc9
       </Router>
     </Suspense>
   )
